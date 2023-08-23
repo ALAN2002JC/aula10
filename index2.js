59e7b59ec26defaea83ca9a089f8cea3a4f1bfb3
ALAN2002JC <141837158+ALAN2002JC@users.noreply.github.com>
2023-08-23 11:58:05 -0300

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,23 @@
+
+/**
+ *
+ * Fa�a um programa que contenha um array com 5 das suas comidas preferidas, armazenado em uma vari�vel.
+ * Em seguida, siga os passos:
+ *
+ * 1. Imprima no console o array completo.
+ *
+ * 2. Imprima no console a mensagem "Essas s�o as minhas comidas preferidas" seguida por cada uma das comidas, uma embaixo da outra.
+ *
+ * 3. Desafio: pergunte ao usu�rio uma comida preferida. Troque a segunda comida da sua lista pela resposta do usu�rio e imprima no console a nova lista.
+ *
+**/
+
+let comida = ['macarrao', 'lasanha', 'estrogonof', 'churrasco', 'pastel' ]
+console.log(comida)
+console.log('essas sao minhas comidas favoritas')
+console.log(comida[0])
+console.log(comida[1])
+console.log(comida[2])
+console.log(comida[3])
+console.log(comida[4])
+
